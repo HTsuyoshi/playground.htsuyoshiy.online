22eb57241f3eb7d9709bd7b8639298fd3d1391ef
HTsuyoshi <henriquetsuyoshiy@gmail.com>
2023-05-11 17:40:19 -0300

    main page prototype

---
+++ home/src/Home.vue
@@ -0,0 +1,33 @@
+<script setup lang='ts'>
+</script>
+
+<template>
+	<div id='home'>
+		<h1>
+			Front-end Playground
+		</h1>
+		<ul>
+			<li>
+				<a href='/canvas' target='_blank'>Pure canvas projects</a>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<style scoped>
+#home {
+	width: 100vw;
+	height: 100vh;
+
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+
+	color: #eeeeee;
+	background-color: #222222;
+}
+ul {
+	list-style: none;
+}
+</style>
